bafbbeb4987485f68a001784e69d286d6f56bc3b
Jeffry Gaston <gastoj3@gmail.com>
2021-05-15 20:47:39 +0100

    new Xamarin project created in Visual Studio 2019

---
+++ Xamarin5Test/Xamarin5Test/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Xamarin5Test.Services;
+using Xamarin5Test.Views;
+
+namespace Xamarin5Test
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
